b324a075da97accfda7fc9be177c61fa8bfb2bbc
Mariángel <126704306+MariangelQuiceno@users.noreply.github.com>
2023-11-13 13:48:46 -0600

    Update areaTriangulo.test.js

--- FuncionTriangulo/areaTriangulo.test.js
+++ FuncionTriangulo/areaTriangulo.test.js
@@ -2,5 +2,5 @@
 const areaTriangulo = require('./areaTriangulo');
 
 test('Base 10 Altura 20 / 2 es 100', () => {
-    expect(areaTriangulo(10, 20)).toBe(120);
+    expect(areaTriangulo(10, 20)).toBe(100);
 });
